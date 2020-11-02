55a22562da3b0428b3b76518a77a42b3bcd3b3f5
fast-geek <rodiongudz0@gmail.com>
2020-11-03 01:30:38 +0300

    Added ping module

---
+++ ping.py
@@ -0,0 +1,16 @@
+from telethon import events
+from datetime import datetime
+from uniborg.util import admin_cmd
+from time import sleep
+
+
+@borg.on(admin_cmd("ping"))
+async def _(event):
+    if event.fwd_from:
+        return
+    start = datetime.now()
+    await event.edit("`Ping checking...`")
+    end = datetime.now()
+    ms = (end - start).microseconds / 1000
+    sleep(0.5)
+    await event.edit("**Ping:**\n`{}ms`".format(ms))
