16ba3950ac981df4905a1b67e0b3de34a080c74e
fast-geek <rodiongudz0@gmail.com>
2020-11-03 12:30:21 +0300

    Modules update

--- ping.py
+++ ping.py
@@ -1,16 +1,21 @@
 from telethon import events
 from datetime import datetime
 from uniborg.util import admin_cmd
+from .. import loader, utils
 from time import sleep
 
+logger = logging.getLogger(__name__)
 
 @borg.on(admin_cmd("ping"))
-async def _(event):
-    if event.fwd_from:
-        return
-    start = datetime.now()
-    await event.edit("`Ping checking...`")
-    end = datetime.now()
-    ms = (end - start).microseconds / 1000
-    sleep(0.5)
-    await event.edit("**Ping:** `{}ms`".format(ms))
+class PingMod(loader.Module):
+    strings = {"name": "Ping"}
+
+    async def _(self):
+        if self.fwd_from:
+            return
+        start = datetime.now()
+        await self.edit("`Ping checking...`")
+        end = datetime.now()
+        ms = (end - start).microseconds / 1000
+        sleep(0.5)
+        await self.edit("**Ping:** `{}ms`".format(ms))
