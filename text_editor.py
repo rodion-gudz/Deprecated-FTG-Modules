af992b7213800ec245714b2be136e10d719d866d
fast-geek <rodiongudz0@gmail.com>
2020-11-05 00:39:49 +0300

    Modules update

--- text_editor.py
+++ text_editor.py
@@ -68,6 +68,38 @@ class TyperMod(loader.Module):
             except:
                 await message.edit(self.strings["msg_is_emp"])
 
+        async def switchcmd(self, message):
+            """Если ты допустил ошибку и набрал текст не сменив раскладку клавиатуры
+    то вернись в его начало и допиши `.switch` и твой текст станет читабельным.
+    Если ты всё же отправил сообщение не в той расскладке, то просто ответь на него этой командой и он измениться.
+    если же твой собеседник допустил ошибку, то просто ответь на его сообщение и сообщение с командой измениться."""
+            RuKeys = """ёйцукенгшщзхъфывапролджэячсмитьбю.Ё"№;%:?ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭ/ЯЧСМИТЬБЮ,"""
+            EnKeys = """`qwertyuiop[]asdfghjkl;'zxcvbnm,./~@#$%^&QWERTYUIOP{}ASDFGHJKL:"|ZXCVBNM<>?"""
+
+            if message.is_reply:
+                reply = await message.get_reply_message()
+                text = reply.raw_text
+                if not text:
+                    await message.edit('Тут текста нету...')
+                    return
+                change = str.maketrans(RuKeys + EnKeys, EnKeys + RuKeys)
+                text = str.translate(text, change)
+
+                if message.from_id != reply.from_id:
+                    await message.edit(text)
+                else:
+                    await message.delete()
+                    await reply.edit(text)
+
+            else:
+                text = utils.get_args_raw(message)
+                if not text:
+                    await message.edit('Тут текста нету...')
+                    return
+                change = str.maketrans(RuKeys + EnKeys, EnKeys + RuKeys)
+                text = str.translate(text, change)
+                await message.edit(text)
+
 async def update_message(message, m, entities):
     try:
         return await utils.answer(message, m, parse_mode=lambda t: (t, entities))
