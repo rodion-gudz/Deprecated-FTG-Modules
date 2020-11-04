2138f4b4e85ff9af2e9203cf7f7126ac470ead8c
fast-geek <rodiongudz0@gmail.com>
2020-11-05 00:34:37 +0300

    Modules update

--- admin_tools.py
+++ admin_tools.py
@@ -13,6 +13,8 @@ from telethon.tl.functions.messages import (UpdatePinnedMessageRequest, EditChat
 from asyncio import sleep
 import random
 logger = logging.getLogger(__name__)
+import telethon
+
 
 # ================== КОНСТАНТЫ ========================
 
@@ -631,6 +633,70 @@ class AdminToolsMod(loader.Module):
             await delus.edit('<b>Кикнуто {} удалённых аккаунтов.\n'
                              'Флудвейт {} секунд вызван ошибкой "Изменение информации".</b>'.format(del_u, e.seconds))
             return
+
+    @loader.group_admin_delete_messages
+    @loader.ratelimit
+    async def purgecmd(self, message):
+        """Purge from the replied message"""
+        if not message.is_reply:
+            await utils.answer(message, self.strings("from_where", message))
+            return
+
+        from_users = set()
+        args = utils.get_args(message)
+        for arg in args:
+            try:
+                entity = await message.client.get_entity(arg)
+                if isinstance(entity, telethon.tl.types.User):
+                    from_users.add(entity.id)
+            except ValueError:
+                pass
+
+        msgs = []
+        from_ids = set()
+        if await message.client.is_bot():
+            if not message.is_channel:
+                await utils.answer(message, self.strings("not_supergroup_bot", message))
+                return
+            for msg in range(message.reply_to_msg_id, message.id + 1):
+                msgs.append(msg)
+                if len(msgs) >= 99:
+                    logger.debug(msgs)
+                    await message.client.delete_messages(message.to_id, msgs)
+                    msgs.clear()
+        else:
+            async for msg in message.client.iter_messages(
+                    entity=message.to_id,
+                    min_id=message.reply_to_msg_id - 1,
+                    reverse=True):
+                if from_users and msg.from_id not in from_users:
+                    continue
+                msgs.append(msg.id)
+                from_ids.add(msg.from_id)
+                if len(msgs) >= 99:
+                    logger.debug(msgs)
+                    await message.client.delete_messages(message.to_id, msgs)
+                    msgs.clear()
+        if msgs:
+            logger.debug(msgs)
+            await message.client.delete_messages(message.to_id, msgs)
+        await self.allmodules.log("purge", group=message.to_id, affected_uids=from_ids)
+
+    @loader.group_admin_delete_messages
+    @loader.ratelimit
+    async def delcmd(self, message):
+        msgs = [message.id]
+        if not message.is_reply:
+            if await message.client.is_bot():
+                await utils.answer(message, self.strings("delete_what", message))
+                return
+            msg = await message.client.iter_messages(message.to_id, 1, max_id=message.id).__anext__()
+        else:
+            msg = await message.get_reply_message()
+        msgs.append(msg.id)
+        logger.debug(msgs)
+        await message.client.delete_messages(message.to_id, msgs)
+        await self.allmodules.log("delete", group=message.to_id, affected_uids=[msg.from_id])
 
 
 def resize(reply):
