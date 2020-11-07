18c782385615096a4ef5eedee6e02f9179386b2f
fast-geek <rodiongudz0@gmail.com>
2020-11-07 16:29:20 +0300

    Modules update

--- photo_editing.py
+++ photo_editing.py
@@ -17,6 +17,11 @@ import random
 from PIL import Image
 import io
 from asyncio import sleep
+from telethon import functions, types
+from asyncio import sleep
+from .. import loader, utils
+from PIL import Image
+import io
 
 _C = 'png'
 _B = 'name'
@@ -653,6 +658,33 @@ class DistortMod(loader.Module):
         await message.delete()
 
         await message.client.send_file(message.to_id, media)
+
+    async def crosscmd(self, message):
+        reply = await message.get_reply_message()
+        if not reply:
+            await message.edit("<b>А что зачеркнуть?</b>")
+            return
+        client = message.client
+        im = io.BytesIO()
+        await client.download_file(reply, im)
+        im = Image.open(im)
+        width, heigth = im.size
+        center = min(width, heigth)
+        im = im.crop(((width - center) // 2, (heigth - center) // 2, (width + center) // 2, (heigth + center) // 2))
+        line = Image.new("RGBA", (width, heigth // 6), (255, 0, 0, 255))
+        line_one = line.rotate(45, expand=True, fillcolor=None).resize(im.size)
+        line_two = line.rotate(-45, expand=True, fillcolor=None).resize(im.size)
+        line_two.paste(line_one, (0, 0), line_one)
+        cross = Image.new("RGBA", im.size, (255, 255, 255, 255))
+        cross.paste(line_two, (0, 0), line_two)
+        cross.putalpha(150)
+        im.paste(cross, (0, 0), cross)
+        output = io.BytesIO()
+        output.name = "output.png"
+        im.save(output)
+        output.seek(0)
+        await client.send_file(message.to_id, output, reply_to=reply)
+        await message.delete()
 
 
 async def KZD(message, type):
