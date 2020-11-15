93332e5b373482ec516ef6e2b9d7608e34884dd9
fast-geek <rodiongudz.services@gmail.com>
2020-11-15 13:42:46 +0300

    Modules update

--- photo_editing.py
+++ photo_editing.py
@@ -9,11 +9,6 @@ import random
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 import logging
-import io
-from telethon.tl.types import DocumentAttributeFilename
-import logging
-from wand.image import Image
-from PIL import Image as IM
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 from .. import loader, utils
@@ -174,41 +169,35 @@ class DistortMod(loader.Module):
     async def distortcmd(self, message):
         if message.is_reply:
             reply_message = await message.get_reply_message()
-            data, mime = await check_medi(reply_message)
+            data = await check_media(reply_message)
             if isinstance(data, bool):
-                await utils.answer(message, "<code>Reply to image or stick!</code>")
+                await message.edit("Error")
                 return
         else:
-            await utils.answer(message, "<code>Reply to image or stick!</code>")
+            await message.edit("Error")
             return
-        rescale_rate = 70
-        a = utils.get_args(message)
-        force_file = False
-        if a:
-            if 'im' in a:
-                force_file = True
-                a.remove('im')
-            if len(a) > 0:
-                if a[0].isdigit():
-                    rescale_rate = int(a[0])
-                    if rescale_rate <= 0:
-                        rescale_rate = 70
+        await message.edit("Distorting...")
+        for distorted in glob.glob("distorted*"):
+            os.remove(distorted)
+        for findistorted in glob.glob("*/distorted*"):
+            os.remove(findistorted)
 
-        await message.edit("<b>Distorting...</b>")
-        file = await message.client.download_media(data, bytes)
-        file, img = io.BytesIO(file), io.BytesIO()
-        img.name = 'img.png'
-        IM.open(file).save(img, 'PNG')
-        media = await distort(io.BytesIO(img.getvalue()), rescale_rate)
-        out, im = io.BytesIO(), IM.open(media)
-        if force_file:
-            mime = 'png'
-        out.name = f'out.{mime}'
-        im.save(out, mime.upper())
-        out.seek(0)
-        await message.edit("<code>S e n d i n g . . .</code>")
-        await message.client.send_file(message.to_id, out, reply_to=reply_message.id)
+        fname = f"distorted{random.randint(1, 100)}.png"
 
+        with open(fname, "wb") as file:
+            file.write(await message.client.download_media(data, bytes))
+        image = Image.open(fname)
+        image.save(fname)
+        imgdimens = image.width, image.height
+        distortcmd = f"convert {fname} -liquid-rescale 60x60%! -resize {imgdimens[0]}x{imgdimens[1]}\! {fname}"
+        os.system(distortcmd)
+        image = Image.open(f"{fname}")
+        buf = io.BytesIO()
+        buf.name = 'image.png'
+        image.save(buf, 'PNG')
+        buf.seek(0)
+        await message.edit("Sending...")
+        await message.client.send_file(message.chat_id, buf, reply_to=reply_message.id)
         await message.delete()
 
     async def linescmd(self, message):
@@ -1024,41 +1013,3 @@ async def dotify(message, reply, pix, mode):
     out.seek(0)
     await reply.reply(file=out)
     await message.delete()
-
-async def distort(file, rescale_rate):
-    img = Image(file=file)
-    x, y = img.size[0], img.size[1]
-    popx = int(rescale_rate*(x//100))
-    popy = int(rescale_rate*(y//100))
-    img.liquid_rescale(popx, popy, delta_x=1, rigidity=0)
-    img.resize(x, y)
-    out = io.BytesIO()
-    out.name = f'output.png'
-    img.save(file=out)
-    return io.BytesIO(out.getvalue())
-
-async def check_medi(reply_message):
-    mime = None
-    if reply_message and reply_message.media:
-        if reply_message.photo:
-            data = reply_message.photo
-            mime = 'image/jpeg'
-        elif reply_message.document:
-            if DocumentAttributeFilename(file_name='AnimatedSticker.tgs') in reply_message.media.document.attributes:
-                return False, mime
-            if reply_message.gif or reply_message.video or reply_message.audio or reply_message.voice:
-                return False, mime
-            data = reply_message.media.document
-            mime = reply_message.media.document.mime_type
-            if 'image/' not in mime:
-                return False, mime
-        else:
-            return False, mime
-    else:
-        return False, mime
-
-    if not data or data is None:
-        return False, mime
-    else:
-        mime = mime.split('/')[1]
-        return data, mime
