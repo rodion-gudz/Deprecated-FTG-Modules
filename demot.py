53f0e087c2d55cac005aa1fe276db9271db107db
fast-geek <rodiongudz.services@gmail.com>
2020-11-17 00:09:42 +0300

    Module update

--- demot.py
+++ demot.py
@@ -90,6 +90,45 @@ class DeMoTiVaToRsMod(loader.Module):
         await message.client.send_file(message.to_id, output, reply_to=reply)
         await message.delete()
 
+    async def nqcmd(self, message):
+        chat = "@ShittyQuoteBot"
+        text = utils.get_args_raw(message)
+        reply = await message.get_reply_message()
+        if not text and not reply:
+            await message.edit("<b>Нет текста или реплая.</b>")
+            return
+        await message.edit("<b>Минуточку...</b>")
+        async with message.client.conversation(chat) as conv:
+            if text:
+                try:
+                    response = conv.wait_event(events.NewMessage(incoming=True, from_users=1389323591))
+                    await message.client.send_message(chat, text)
+                    response = await response
+                except YouBlockedUserError:
+                    await message.edit("<b>Разблокируй @ShittyQuoteBot</b>")
+                    return
+            if reply:
+                try:
+                    user = await utils.get_user(reply)
+                    response = conv.wait_event(events.NewMessage(incoming=True, from_users=1389323591))
+                    await message.client.send_message(chat, f"{reply.raw_text} (с) {user.first_name}")
+                    response = await response
+                except YouBlockedUserError:
+                    await message.edit("<b>Разблокируй @ShittyQuoteBot</b>")
+                    return
+        if response.text:
+            await message.client.send_message(message.to_id, f"<b> {response.text}</b>")
+            await message.delete()
+        if response.media:
+            await message.client.send_file(message.to_id, response.media)
+            await message.delete()
+        await message.client(functions.messages.DeleteHistoryRequest(
+            peer='ShittyQuoteBot',
+            max_id=0,
+            just_clear=False,
+            revoke=True
+        ))
+
     async def centercmd(self, message):
         cols = {'white': 1, 'whit': 1, 'whi': 1, 'wh': 1, 'w': 1,
                 'black': 2, 'blac': 2, 'bla': 2, 'bl': 2, 'b': 2}
