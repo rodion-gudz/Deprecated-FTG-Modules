fad5982daf181d2fea5543ca225b3b274a8d8799
fast-geek <rodiongudz.services@gmail.com>
2020-11-22 16:59:28 +0300

    Modules update

--- quote.py
+++ quote.py
@@ -506,47 +506,47 @@ class mQuotesMod(loader.Module):
 				)
 			)
             
-    async def stextcmd(self, message):
-        await message.delete()
-        text = utils.get_args_raw(message)
-        reply = await message.get_reply_message()
-        if not text:
-            if not reply:
-                text = "#ffffff .stext <text or reply>"
-            elif not reply.message:
-                text = "#ffffff .stext <text or reply>"
-            else:
-                text = reply.raw_text
-        color = text.split(" ", 1)[0]
-        if color.startswith("#") and len(color) == 7:
-            for ch in color.lower()[1:]:
-                if ch not in "0123456789abcdef":
-                    break
-            if len(text.split(" ", 1)) > 1:
-                text = text.split(" ", 1)[1]
-            else:
-                if reply:
-                    if reply.message:
-                        text = reply.raw_text
-        else:
-            color = "#FFFFFF"
-        txt = []
-        for line in text.split("\n"):
-            txt.append("\n".join(wrap(line, 30)))
-        text = "\n".join(txt)
-        font = io.BytesIO(bytes_font)
-        font = ImageFont.truetype(font, 100)
-        image = Image.new("RGBA", (1, 1), (0, 0, 0, 0))
-        draw = ImageDraw.Draw(image)
-        w, h = draw.multiline_textsize(text=text, font=font)
-        image = Image.new("RGBA", (w + 100, h + 100), (0, 0, 0, 0))
-        draw = ImageDraw.Draw(image)
-        draw.multiline_text((50, 50), text=text, font=font, fill=color, align="center")
-        output = io.BytesIO()
-        output.name = color + ".webp"
-        image.save(output, "WEBP")
-        output.seek(0)
-        await self.client.send_file(message.to_id, output, reply_to=reply)
+		async def stextcmd(self, message):
+			await message.delete()
+			text = utils.get_args_raw(message)
+			reply = await message.get_reply_message()
+			if not text:
+				if not reply:
+					text = "#ffffff .stext <text or reply>"
+				elif not reply.message:
+					text = "#ffffff .stext <text or reply>"
+				else:
+					text = reply.raw_text
+			color = text.split(" ", 1)[0]
+			if color.startswith("#") and len(color) == 7:
+				for ch in color.lower()[1:]:
+					if ch not in "0123456789abcdef":
+						break
+				if len(text.split(" ", 1)) > 1:
+					text = text.split(" ", 1)[1]
+				else:
+					if reply:
+						if reply.message:
+							text = reply.raw_text
+			else:
+				color = "#FFFFFF"
+			txt = []
+			for line in text.split("\n"):
+				txt.append("\n".join(wrap(line, 30)))
+			text = "\n".join(txt)
+			font = io.BytesIO(bytes_font)
+			font = ImageFont.truetype(font, 100)
+			image = Image.new("RGBA", (1, 1), (0, 0, 0, 0))
+			draw = ImageDraw.Draw(image)
+			w, h = draw.multiline_textsize(text=text, font=font)
+			image = Image.new("RGBA", (w + 100, h + 100), (0, 0, 0, 0))
+			draw = ImageDraw.Draw(image)
+			draw.multiline_text((50, 50), text=text, font=font, fill=color, align="center")
+			output = io.BytesIO()
+			output.name = color + ".webp"
+			image.save(output, "WEBP")
+			output.seek(0)
+			await self.client.send_file(message.to_id, output, reply_to=reply)
 
 async def clean_files():
 	return os.system("rm -rf mishase_cache/*")
