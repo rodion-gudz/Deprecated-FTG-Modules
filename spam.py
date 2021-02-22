7eab48ac31e196563e01b670a4906e7c085e0426
fast-geek <rodiongudz.services@gmail.com>
2021-02-22 12:46:09 +0300

    Modules update

--- spam.py
+++ spam.py
@@ -1,39 +1,98 @@
 from .. import loader, utils
 import logging
-import asyncio
+from asyncio import sleep, gather
 
 
-logger = logging.getLogger(__name__)
+def register(cb):
+    cb(SpamMod())
 
 
 @loader.tds
 class SpamMod(loader.Module):
-    """Annoys people really effectively"""
-    strings = {"name": "Spam"}
+    """Спам модуль"""
+    strings = {'name': 'Spam'}
 
     async def spamcmd(self, message):
-        """.spam <count> <message>"""
-        use_reply = False
-        args = utils.get_args(message)
-        logger.debug(args)
-        count = int(args[0].strip())
+        """Обычный спам. Используй .spam <кол-во:int> <текст или реплай>."""
+        try:
+            await message.delete()
+            args = utils.get_args(message)
+            count = int(args[0].strip())
+            reply = await message.get_reply_message()
+            if reply:
+                if reply.media:
+                    for _ in range(count):
+                        await message.client.send_file(message.to_id, reply.media)
+                    return
+                else:
+                    for _ in range(count):
+                        await message.client.send_message(message.to_id, reply)
+            else:
+                message.message = " ".join(args[1:])
+                for _ in range(count):
+                    await gather(*[message.respond(message)])
+        except:
+            return await message.client.send_message(message.to_id, '.spam <кол-во:int> <текст или реплай>.')
+
+    async def cspamcmd(self, message):
+        """Спам символами. Используй .cspam <текст или реплай>."""
+        await message.delete()
         reply = await message.get_reply_message()
         if reply:
-            if reply.media:
-                media = reply.media
-                await message.delete()
-                for _ in range(count):
-                    await message.client.send_file(message.to_id, media)
-                return
-            else:
-                text = reply
-                await message.delete()
-                for _ in range(count):
-                    await message.client.send_message(message.to_id, text)
+            msg = reply.text
         else:
-            spam = message
-            spam.message = " ".join(args[1:])
-            await message.delete()
-            for i in range(count):
-                await asyncio.gather(*[message.respond(spam)])
+            msg = utils.get_args_raw(message)
+        msg = msg.replace(' ', '')
+        for m in msg:
+            await message.respond(m)
 
+    async def wspamcmd(self, message):
+        """Спам словами. Используй .wspam <текст или реплай>."""
+        await message.delete()
+        reply = await message.get_reply_message()
+        if reply:
+            msg = reply.text
+        else:
+            msg = utils.get_args_raw(message)
+        msg = msg.split()
+        for m in msg:
+            await message.respond(m)
+
+    async def delayspamcmd(self, message):
+        """Спам с задержкой. Используй .delayspam <время:int> <кол-во:int> <текст или реплай>."""
+        try:
+            await message.delete()
+            args = utils.get_args_raw(message)
+            reply = await message.get_reply_message()
+            time = int(args.split(' ', 2)[0])
+            count = int(args.split(' ', 2)[1])
+            if reply:
+                if reply.media:
+                    for _ in range(count):
+                        await message.client.send_file(message.to_id, reply.media, reply_to=reply.id)
+                        await sleep(time)
+                else:
+                    for _ in range(count):
+                        await reply.reply(args.split(' ', 2)[2])
+                        await sleep(time)
+            else:
+                spammsg = args.split(' ', 2)[2]
+                for _ in range(count):
+                    await message.respond(spammsg)
+                    await sleep(time)
+        except:
+            return await message.client.send_message(message.to_id,
+                                                     '.delayspam <время:int> <кол-во:int> <текст или реплай>')
+
+    async def replayspamcmd(self, message):
+        """Спам в ответ на сообщение. Используй ответом на сообщение .replayspam <кол-во:int> <текст>."""
+        try:
+            await message.delete()
+            args = utils.get_args_raw(message)
+            count = int(args.split(' ', 2)[0])
+            reply = await message.get_reply_message()
+            for _ in range(count):
+                await reply.reply(args.split(' ', 2)[1])
+            return
+        except:
+            return await message.client.send_message(message.to_id, '.replayspam <кол-во:int> <текст>.')
