82285134d5a8accbcad3fdc8f5f7530364530795
fast-geek <rodiongudz.services@gmail.com>
2021-02-26 11:38:56 +0300

    Modules update

--- afk.py
+++ afk.py
@@ -1,3 +1,19 @@
+#    Friendly Telegram (telegram userbot)
+#    Copyright (C) 2018-2019 The Authors
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from .. import loader, utils
 
 import logging
@@ -11,6 +27,7 @@ logger = logging.getLogger(__name__)
 
 @loader.tds
 class AFKMod(loader.Module):
+    """Provides a message saying that you are unavailable"""
     strings = {"name": "AFK",
                "gone": "<b>I'm goin' AFK</b>",
                "back": "<b>I'm no longer AFK</b>",
@@ -18,36 +35,56 @@ class AFKMod(loader.Module):
                "afk_reason": "<b>I'm AFK right now (since {} ago).\nReason:</b> <i>{}</i>"}
 
     async def client_ready(self, client, db):
-            self._db = db
-            self._me = await client.get_me()
-    
+        self._db = db
+        self._me = await client.get_me()
+
     async def afkcmd(self, message):
-            """.afk [текст]"""
-            if utils.get_args_raw(message):
-                self._db.set(__name__, "afk", utils.get_args_raw(message))
-            else:
-                self._db.set(__name__, "afk", True)
-            self._db.set(__name__, "gone", time.time())
-            await self.allmodules.log("afk", data=utils.get_args_raw(message) or None)
-            await utils.answer(message, self.strings("gone", message))
-    
+        """.afk [message]"""
+        if utils.get_args_raw(message):
+            self._db.set(__name__, "afk", utils.get_args_raw(message))
+        else:
+            self._db.set(__name__, "afk", True)
+        self._db.set(__name__, "gone", time.time())
+        self._db.set(__name__, "ratelimit", [])
+        await self.allmodules.log("afk", data=utils.get_args_raw(message) or None)
+        await utils.answer(message, self.strings("gone", message))
+
     async def unafkcmd(self, message):
-            """Перестаёт писать"""
-            self._db.set(__name__, "afk", False)
-            self._db.set(__name__, "gone", None)
-            await self.allmodules.log("unafk")
-            await utils.answer(message, self.strings("back", message))
-    
+        """Remove the AFK status"""
+        self._db.set(__name__, "afk", False)
+        self._db.set(__name__, "gone", None)
+        self._db.set(__name__, "ratelimit", [])
+        await self.allmodules.log("unafk")
+        await utils.answer(message, self.strings("back", message))
+
     async def watcher(self, message):
-            if not isinstance(message, types.Message):
+        if not isinstance(message, types.Message):
+            return
+        if message.mentioned or getattr(message.to_id, "user_id", None) == self._me.id:
+            afk_state = self.get_afk()
+            if not afk_state:
                 return
-            if message.mentioned or getattr(message.to_id, "user_id", None) == self._me.id:
-                if self.get_afk() != False:
-                    afk_state = self.get_afk()
-                    ret = self.strings("afk_reason", message).format(afk_state)
-                    await utils.answer(message, ret)
-                else:
-                    return
-    
-def get_afk(self):
-            return self._db.get(__name__, "afk", False)
+            logger.debug("tagged!")
+            ratelimit = self._db.get(__name__, "ratelimit", [])
+            if utils.get_chat_id(message) in ratelimit:
+                return
+            else:
+                self._db.setdefault(__name__, {}).setdefault("ratelimit", []).append(utils.get_chat_id(message))
+                self._db.save()
+            user = await utils.get_user(message)
+            if user.is_self or user.bot or user.verified:
+                logger.debug("User is self, bot or verified.")
+                return
+            if self.get_afk() is False:
+                return
+            now = datetime.datetime.now().replace(microsecond=0)
+            gone = datetime.datetime.fromtimestamp(self._db.get(__name__, "gone")).replace(microsecond=0)
+            diff = now - gone
+            if afk_state is True:
+                ret = self.strings("afk", message).format(diff)
+            elif afk_state is not False:
+                ret = self.strings("afk_reason", message).format(diff, afk_state)
+            await utils.answer(message, ret, reply_to=message)
+
+    def get_afk(self):
+        return self._db.get(__name__, "afk", False)
