18f7110ecdd8658ef419535dc495308fff45e0de
fast-geek <rodiongudz.services@gmail.com>
2021-02-28 12:36:25 +0300

    Modules update

--- afk.py
+++ afk.py
@@ -1,29 +1,12 @@
-#    Friendly Telegram (telegram userbot)
-#    Copyright (C) 2018-2019 The Authors
-
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
+import time
 from .. import loader, utils
 
 import logging
-import datetime
-import time
+
 
 from telethon import types
 
 logger = logging.getLogger(__name__)
-
 
 @loader.tds
 class AFKMod(loader.Module):
@@ -38,7 +21,7 @@ class AFKMod(loader.Module):
         self._db = db
         self._me = await client.get_me()
 
-    async def afkcmd(self, message):
+    async def rfdcmd(self, message):
         """.afk [message]"""
         if utils.get_args_raw(message):
             self._db.set(__name__, "afk", utils.get_args_raw(message))
@@ -48,7 +31,7 @@ class AFKMod(loader.Module):
         await self.allmodules.log("afk", data=utils.get_args_raw(message) or None)
         await utils.answer(message, self.strings("gone", message))
 
-    async def unafkcmd(self, message):
+    async def unrfdcmd(self, message):
         """Remove the AFK status"""
         self._db.set(__name__, "afk", False)
         self._db.set(__name__, "gone", None)
@@ -59,7 +42,7 @@ class AFKMod(loader.Module):
         if not isinstance(message, types.Message):
             return
         if message.mentioned or getattr(message.to_id, "user_id", None) == self._me.id:
-            if self.get_afk():
+            if self.get_afk() != False:
                 afk_state = self.get_afk()
                 ret = self.strings("afk_reason", message).format(afk_state)
                 await utils.answer(message, ret)
