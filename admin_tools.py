799246f790ebe3a3c5726166af0c3359303e033c
fast-geek <rodiongudz.services@gmail.com>
2021-03-02 19:00:58 +0300

    Modules update

--- admin_tools.py
+++ admin_tools.py
@@ -4,12 +4,16 @@
 
 import io, time
 from .. import loader, utils, security
+import logging
 from PIL import Image
+import telethon
 from telethon.errors import ChatAdminRequiredError, UserAdminInvalidError, PhotoCropSizeSmallError
 from telethon.tl.types import ChatAdminRights, ChatBannedRights
 from telethon.tl.functions.channels import EditAdminRequest, EditBannedRequest, EditPhotoRequest, \
     DeleteUserHistoryRequest
 from telethon.tl.functions.messages import EditChatAdminRequest
+
+logger = logging.getLogger(__name__)
 
 # ================== КОНСТАНТЫ ========================
 
@@ -103,7 +107,10 @@ class AdminToolsMod(loader.Module):
                'no_args_or_reply': '<b>Нет аргументов или реплая.</b>',
                'deleted': '<b>Все сообщения от {} удалены.</b>',
                'del_u_search': '<b>Поиск удалённых аккаунтов...</b>',
-               'del_u_kicking': '<b>Кик удалённых аккаунтов...\nОх~, я могу это сделать?!</b>'}
+               'del_u_kicking': '<b>Кик удалённых аккаунтов...\nОх~, я могу это сделать?!</b>',
+               "from_where": "<b>Which messages should be purged?</b>",
+               "not_supergroup_bot": "<b>Purges can only take place in supergroups</b>",
+               "delete_what": "<b>What message should be deleted?</b>"}
 
     async def ecpcmd(self, message):
         """Команда .ecp изменяет картинку чата.\nИспользование: .ecp <реплай на картинку/стикер>."""
@@ -508,6 +515,71 @@ class AdminToolsMod(loader.Module):
             del_status = f"<b>Кикнуто {del_u} удалённых аккаунтов.\n" \
                          f"{del_a} удалённых аккаунта админов не кикнуты.</b>"
         await message.edit(del_status)
+
+    @loader.group_admin_delete_messages
+    @loader.ratelimit
+    async def purgecmd(self, message):
+        """Purge from the replied message"""
+        if not message.is_reply:
+            await utils.answer(message, self.strings("from_where", message))
+            return
+
+        from_users = set()
+        args = utils.get_args(message)
+        for arg in args:
+            try:
+                entity = await message.client.get_entity(arg)
+                if isinstance(entity, telethon.tl.types.User):
+                    from_users.add(entity.id)
+            except ValueError:
+                pass
+
+        msgs = []
+        from_ids = set()
+        if await message.client.is_bot():
+            if not message.is_channel:
+                await utils.answer(message, self.strings("not_supergroup_bot", message))
+                return
+            for msg in range(message.reply_to_msg_id, message.id + 1):
+                msgs.append(msg)
+                if len(msgs) >= 99:
+                    logger.debug(msgs)
+                    await message.client.delete_messages(message.to_id, msgs)
+                    msgs.clear()
+        else:
+            async for msg in message.client.iter_messages(
+                    entity=message.to_id,
+                    min_id=message.reply_to_msg_id - 1,
+                    reverse=True):
+                if from_users and msg.from_id not in from_users:
+                    continue
+                msgs.append(msg.id)
+                from_ids.add(msg.from_id)
+                if len(msgs) >= 99:
+                    logger.debug(msgs)
+                    await message.client.delete_messages(message.to_id, msgs)
+                    msgs.clear()
+        if msgs:
+            logger.debug(msgs)
+            await message.client.delete_messages(message.to_id, msgs)
+        await self.allmodules.log("purge", group=message.to_id, affected_uids=from_ids)
+
+    @loader.group_admin_delete_messages
+    @loader.ratelimit
+    async def delcmd(self, message):
+        """Delete the replied message"""
+        msgs = [message.id]
+        if not message.is_reply:
+            if await message.client.is_bot():
+                await utils.answer(message, self.strings("delete_what", message))
+                return
+            msg = await message.client.iter_messages(message.to_id, 1, max_id=message.id).__anext__()
+        else:
+            msg = await message.get_reply_message()
+        msgs.append(msg.id)
+        logger.debug(msgs)
+        await message.client.delete_messages(message.to_id, msgs)
+        await self.allmodules.log("delete", group=message.to_id, affected_uids=[msg.from_id])
 
 
 def resizepic(reply):
