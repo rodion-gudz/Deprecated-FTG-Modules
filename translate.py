8e470e5c949a8d66ca79ad8a9499dd54eb6dc5a8
fast-geek <rodiongudz.services@gmail.com>
2021-03-06 00:29:05 +0300

    Modules update

--- translate.py
+++ translate.py
@@ -1,29 +1,55 @@
-import logging
+# -*- coding: utf-8 -*-
+
+# Module author: @ftgmodulesbyfl1yd
+
+# requires: googletrans==4.0.0rc1
 
 from .. import loader, utils
-
-logger = logging.getLogger(__name__)
-import os
-import time
-import os
+from googletrans import LANGUAGES, Translator
 from telethon import functions
-from telethon import events
-import asyncio
-from telethon.tl.types import DocumentAttributeFilename
 from telethon.errors.rpcerrorlist import YouBlockedUserError
-from requests import get
 from telethon import events
 
 
 @loader.tds
-class TranslateMod(loader.Module):
-    """Translator"""
-    strings = {"name": "Translator",
-               "translated": "<b>From: </b><code>{from_lang}</code>"
-                             "\n<b>To: </b><code>{to_lang}</code>\n\n{output}",
-               "invalid_text": "Invalid text to translate",
-               "doc_default_lang": "Language to translate to by default",
-               "doc_api_key": "API key from https://translate.yandex.com/developers/keys"}
+class TranslatorMod(loader.Module):
+    """Translator Module"""
+    strings = {'name': 'Translate'}
+
+    async def gtrslcmd(self, message):
+        """Используй: .gtrsl <с какого языка перевести> <на какой перевести>
+        <текст> или .gtrsl <на какой перевести> <реплай>; langs. """
+        args = utils.get_args_raw(message)
+        reply = await message.get_reply_message()
+        langs = LANGUAGES
+        lang = args.split()
+        tr = Translator().translate
+        if not args and not reply:
+            return await message.edit("Нет аргументов или реплая.")
+        if args == "langs":
+            return await message.edit("<code>" + '\n'.join(str(langs).split(', ')) + "</code>")
+        if reply:
+            try:
+                trslreply = True
+                text = reply.text
+                if len(lang) >= 2:
+                    trslreply = False
+                dest = langs[lang[0]]
+                r = tr(args.split(' ', 1)[1] if trslreply is False else text, dest=dest)
+            except: r = tr(reply.text)
+        else:
+            try:
+                try:
+                    src = langs[lang[0]]
+                    dest = langs[lang[1]]
+                    text = args.split(' ', 2)[2]
+                    r = tr(text, src=src, dest=dest)
+                except:
+                    dest = langs[lang[0]]
+                    text = args.split(' ', 1)[1]
+                    r = tr(text, dest=dest)
+            except KeyError: r = tr(args)
+        return await message.edit(f"<b>[{r.src} ➜ {r.dest}]</b>\n{r.text}")
 
     @loader.unrestricted
     @loader.ratelimit
@@ -35,13 +61,17 @@ class TranslateMod(loader.Module):
             if reply:
                 text = await event.get_reply_message()
             try:
-                response = conv.wait_event(events.NewMessage(incoming=True, from_users=104784211))
+                response = conv.wait_event(
+                    events.NewMessage(incoming=True, from_users=104784211))
                 mm = await event.client.send_message(chat, text)
                 response = await response
                 await mm.delete()
             except YouBlockedUserError:
-                await event.edit('<code>Разблокируй @ttsavebot</code>')
+                await event.edit('<code>Разблокируй @YTranslateBot</code>')
                 return
             await event.edit(str(response.text).split(": ", 1)[1])
             await event.client(
-                functions.messages.DeleteHistoryRequest(peer='YTranslateBot', max_id=0, just_clear=False, revoke=True))
+                functions.messages.DeleteHistoryRequest(peer='YTranslateBot',
+                                                        max_id=0,
+                                                        just_clear=False,
+                                                        revoke=True))
